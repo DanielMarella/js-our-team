63bde2ca9b123b00f19b51b8bf22150f796337de
DanielMarella <marella.daniel96@gmail.com>
2023-05-29 15:07:25 +0200

    creation of array with information of all user

---
+++ js/myscript.js
@@ -0,0 +1,64 @@
+
+
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+// Wayne Barnett     ounder & CEO      wayne-barnett-founder-ceo.jpg
+// Angela Caroll     Chief Editor      angela-caroll-chief-editor.jpg
+// Walter Gordon     Office Manager    walter-gordon-office-manager.jpg
+// Angela Lopez      Social Media      Manager  angela-lopez-social-media-manager.jpg
+// Scott Estrada     Developer         scott-estrada-developer.jpg
+// Barbara Ramos     Graphic Designer  barbara-ramos-graphic-designer.jpg
+
+
+const team = [];
+
+const userOne = {
+    name : 'Wayne Barnett',
+    role : 'ounder & CEO',
+    profileImg : 'wayne-barnett-founder-ceo.jpg'
+}
+
+const userTwo = {
+    name : 'Angela Caroll',
+    role : 'Chief Editor',
+    profileImg : 'wayne-barnett-founder-ceo.jpg'
+}
+
+const userThree = {
+    name : 'Walter Gordon',
+    role : 'Office Manager',
+    profileImg : 'wayne-barnett-founder-ceo.jpg'
+}
+
+const userFour = {
+    name : 'Angela Lopez ',
+    role : 'Social Media',
+    profileImg : 'wayne-barnett-founder-ceo.jpg'
+}
+
+const userFive = {
+    name : 'Scott Estrada',
+    role : 'Developer',
+    profileImg : 'wayne-barnett-founder-ceo.jpg'
+}
+
+const userSix = {
+    name : 'Barbara Ramos',
+    role : 'Graphic Designer',
+    profileImg : 'wayne-barnett-founder-ceo.jpg'
+}
+
+
+team.push(userOne, userTwo, userThree, userFour, userFive, userSix);
+console.log(team)
